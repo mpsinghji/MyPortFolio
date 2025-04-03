37013a802cd607a85722b38d57dd3ff9706338a5
Manpreet Singh <Manpreet.singhcomet@gmail.com>
2025-04-03 14:32:16 +0530

    commit 15

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,11 +5,11 @@ import "./Projects.css";
 
 const items = [
   { id: 1, img: "/BlogVerse.png", title: "Full Stack Blog App", link: "/" },
-  { id: 2, img: "/PROJECT_ZERO.png", title: "College Management System", link: "/" },
+  { id: 2, img: "/PROJECT_ZERO.png", title: "College Management System", link: "https://mpji-campus-sync.vercel.app/" },
   { id: 3, img: "/Portfolio.png", title: "Animated Portfolio", link: "/" },
-  { id: 4, img: "/RealChat.png", title: "Real-time Chat App", link: "/" },
-  { id: 5, img: "/SocialMedia.png", title: "Social Media Project", link: "/" },
-  { id: 6, img: "/JobPortal.png", title: "Job Portal Website", link: "/" },
+  // { id: 4, img: "/RealChat.png", title: "Real-time Chat App", link: "/" },
+  // { id: 5, img: "/SocialMedia.png", title: "Social Media Project", link: "/" },
+  { id: 4, img: "/JobPortal.png", title: "Job Portal Website", link: "/" },
 ];
 
 const Projects = () => {
